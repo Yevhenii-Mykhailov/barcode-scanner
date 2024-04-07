69d6df95cf0398e03cc875dea31d978673349ea7
YevheniiMih <yevhenii_m@optimove.com>
2024-04-07 17:36:31 +0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Yevhenii M on 07.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
